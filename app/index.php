ef3a5a4b4654b0a3d35613f3c8d1fc47d9b908da
yonks20 <yonkieyudha@gmail.com>
2022-10-24 14:40:16 +0700

    first push datatables

---
+++ app/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include ('header.php'); ?>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Login</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+  <!-- Preloader -->
+  <?php include ('preloader.php'); ?>
+  <!-- Navbar -->
+  <?php include('navbar.php'); ?>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    
+    <?php include ('logo.php') ?>
+
+    <!-- Sidebar -->
+    <?php  include('sidebar.php'); ?>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    
+    
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <?php include ('data_kpi.php'); ?>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <?php include ('content_header.php'); ?>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<?php include ('footer.php'); ?>
+<!-- jQuery -->
+
+</body>
+</html>
---
+++ conf/autentikasi.php
@@ -0,0 +1,24 @@
+<?php
+
+include ('config.php');
+$username =$_POST['username'];
+$password =$_POST['password'];
+
+$query =mysqli_query($koneksi, "SELECT * FROM tb_users WHERE username= '$username' AND password= '$password'");
+
+if(mysqli_num_rows($query)==1){
+    header('location:../app');
+}
+
+
+
+else if ($username == '' || $password == ''){
+
+    header('location:../index.php?error=2');
+}
+else{
+    header('location:../index.php?error=1');
+}
+
+
+?>
